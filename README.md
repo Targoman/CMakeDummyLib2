e0aae2a74fdb182bd005633bc668a09e9d680eb3
Behrooz Vedadian <vedadian@gmail.com>
2022-12-26 18:16:50 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CMakeDummyLibrary2
+This is a dummy library used to create a sample multi dependency application
---
+++ libsrc/libDummyLib2/clsSample.h
@@ -0,0 +1,31 @@
+/*################################################################################
+#   TargomanBuildSystem
+#
+#   Copyright(c) 2021 by Targoman Intelligent Processing <http://tip.co.ir>
+#
+#   Redistribution and use in source and binary forms are allowed under the
+#   terms of BSD License 2.0. See LICENSE file
+################################################################################*/
+/**
+ * @author S. Mohammad M. Ziabary <ziabary@targoman.com>
+ */
+#include <QSharedDataPointer>
+#include "libDummyLib1/clsSample.h"
+
+namespace DummyLib2 {
+namespace Private {
+class clsSamplePrivate;
+}
+class clsSample{
+public:
+    clsSample(const QString& _sampleString);
+    ~clsSample();
+
+    QString info();
+
+public:
+    QSharedDataPointer<Private::clsSamplePrivate> Data;
+    DummyLib1::clsSample SampleFromDummyLib1;
+};
+
+}
